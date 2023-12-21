b1486300fc589d45c1a236a7f6d1ec4a984263c7
mdrahathossenantor <md.rahathossenantor@gmail.com>
2023-12-21 17:11:56 +0600

    Footer section completed

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,24 @@
+
+const Footer = () => {
+    return (
+        <div className="bg-[#7CAB17]">
+            <div className="md:container md:mx-auto 2xl:px-0 xl:px-0 lg:px-5 md:px-5 px-5 pt-10">
+                <footer className="flex justify-between items-center gap-5 text-white">
+                    <div>
+                        <ul className="flex gap-5 open-sans">
+                            <li className="text-[17px]"><a href="">Welcome</a></li>
+                            <li className="text-[17px]"><a href="">Teachers</a></li>
+                            <li className="text-[17px]"><a href="">Contact</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <img src="https://colibriwp-work.colibriwp.com/school-ii/wp-content/uploads/2019/02/house.png" alt="image" />
+                    </div>
+                </footer>
+                <p className="raleway text-center text-white pb-5">Copyright © 2023 - All rights reserved by Atturath Kindergarden.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import About from "../components/About";
 import Featured from "../components/Featured";
 import Features from "../components/Features";
+import Footer from "../components/Footer";
 import SignUpSection from "../components/SignUpSection";
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
             <About></About>
             <Features></Features>
             <SignUpSection></SignUpSection>
+            <Footer></Footer>
         </>
     );
 };
