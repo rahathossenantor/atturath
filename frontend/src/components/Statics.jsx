ece01c7318b9d7cc4f9744a847e27891197b307a
mdrahathossenantor <md.rahathossenantor@gmail.com>
2023-12-24 08:40:44 +0600

    Statistics section completed

---
+++ frontend/src/components/Statics.jsx
@@ -0,0 +1,45 @@
+import like from "../assets/like.png";
+import kid from "../assets/baby-boy.png";
+import degree from "../assets/degree.png";
+import love from "../assets/heart.png";
+
+const Statics = () => {
+    return (
+        <div className="statistics">
+            <div className="xl:py-20 lg:py-16 md:py-12 py-10 bg-[#00000061] text-white">
+                <div className="md:container md:mx-auto 2xl:px-0 xl:px-0 lg:px-5 md:px-5 px-5 grid xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-2 grid-cols-2 gap-7 raleway">
+                    <div className="flex flex-col items-center text-center">
+                        <h5 className="text-2xl mb-2">happy parents</h5>
+                        <div className="flex items-center gap-3">
+                            <img className="inline-block w-12" src={like} alt="icon" />
+                            <h4 className="text-4xl">100</h4>
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-center text-center">
+                        <h5 className="text-2xl mb-2">happy children</h5>
+                        <div className="flex items-center gap-3">
+                            <img className="inline-block w-12" src={kid} alt="icon" />
+                            <h4 className="text-4xl">100</h4>
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-center text-center">
+                        <h5 className="text-2xl mb-2">happy teachers</h5>
+                        <div className="flex items-center gap-3">
+                            <img className="inline-block w-12" src={degree} alt="icon" />
+                            <h4 className="text-4xl">100</h4>
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-center text-center">
+                        <h5 className="text-2xl mb-2">happy hours</h5>
+                        <div className="flex items-center gap-3">
+                            <img className="inline-block w-12" src={love} alt="icon" />
+                            <h4 className="text-4xl">100</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Statics;
--- frontend/src/pages/Teachers.jsx
+++ frontend/src/pages/Teachers.jsx
@@ -1,11 +1,13 @@
 import AllTeachers from "../components/AllTeachers";
 import SectionHeader from "../components/SectionHeader"
+import Statics from "../components/Statics";
 
 const Teachers = () => {
     return (
         <>
             <SectionHeader title="Teachers"></SectionHeader>
             <AllTeachers></AllTeachers>
+            <Statics></Statics>
         </>
     );
 };
