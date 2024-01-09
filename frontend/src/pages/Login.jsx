abd94543e95474bf72c576710023b51f9ed12892
mdrahathossenantor <md.rahathossenantor@gmail.com>
2024-01-09 14:49:03 +0600

    Email pass login implemented

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,32 @@
 import { FaFacebook, FaGoogle } from "react-icons/fa";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import useAuth from "../hooks/useAuth";
+import Swal from "sweetalert2";
 
 const Login = () => {
     const { register, handleSubmit, formState: { errors }, reset } = useForm();
+    const [errStatus, setErrStatus] = useState("");
+    const { emailPassLogin } = useAuth();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const destination = location.state ? location.state : "/";
 
     const onSubmit = async (data) => {
-        console.log(data);
-        reset();
+        emailPassLogin(data.email, data.password)
+            .then(() => {
+                Swal.fire({
+                    title: "Success!",
+                    text: "Login successful!",
+                    icon: "success",
+                    confirmButtonText: "Close",
+                    timer: 2000
+                });
+                reset();
+                navigate(destination);
+            })
+            .catch(err => setErrStatus(err.message));
     };
 
     return (
@@ -28,18 +47,18 @@ const Login = () => {
                                 <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
                                     <div className="space-y-1">
                                         <label htmlFor="email" className="text-sm font-medium">Email <span className="text-red-600">*</span></label>
-                                        <input {...register("email", { required: true })} type="email" id="email" name="email" placeholder="Enter your email" className="block w-full rounded-lg border border-gray-200 px-5 py-3 leading-6 placeholder-gray-500 focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:placeholder-gray-400 dark:focus:border-blue-500"/>
+                                        <input {...register("email", { required: true })} type="email" id="email" name="email" placeholder="Enter your email" className="block w-full rounded-lg border border-gray-200 px-5 py-3 leading-6 placeholder-gray-500 focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:placeholder-gray-400 dark:focus:border-blue-500" />
                                         {errors.email && <span className="text-red-600 mt-1 inline-block">Email is required</span>}
                                     </div>
                                     <div className="space-y-1">
                                         <label htmlFor="password" className="text-sm font-medium">Password <span className="text-red-600">*</span></label>
-                                        <input {...register("password", { required: true })} type="password" id="password" name="password" placeholder="Enter your password" className="block w-full rounded-lg border border-gray-200 px-5 py-3 leading-6 placeholder-gray-500 focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:placeholder-gray-400 dark:focus:border-blue-500"/>
+                                        <input {...register("password", { required: true })} type="password" id="password" name="password" placeholder="Enter your password" className="block w-full rounded-lg border border-gray-200 px-5 py-3 leading-6 placeholder-gray-500 focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:placeholder-gray-400 dark:focus:border-blue-500" />
                                         {errors.password && <span className="text-red-600 mt-1 inline-block">Password is required</span>}
                                     </div>
                                     <div>
                                         <div className="mb-5 flex items-center justify-between space-x-2">
                                             <label className="flex items-center">
-                                                <input type="checkbox" id="remember_me" name="remember_me" className="size-4 rounded border border-gray-200 text-blue-500 focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:ring-offset-gray-900 dark:checked:border-transparent dark:checked:bg-blue-500 dark:focus:border-blue-500"/>
+                                                <input type="checkbox" id="remember_me" name="remember_me" className="size-4 rounded border border-gray-200 text-blue-500 focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:ring-offset-gray-900 dark:checked:border-transparent dark:checked:bg-blue-500 dark:focus:border-blue-500" />
                                                 <span className="ml-2 text-sm">Remember me</span>
                                             </label>
                                             <a href="#" className="inline-block text-sm font-medium text-blue-600 hover:text-blue-400 dark:text-blue-400 dark:hover:text-blue-300">Forgot Password?</a>
@@ -49,22 +68,25 @@ const Login = () => {
                                         </button>
                                         {/* Divider: With Label */}
                                         <div className="my-5 flex items-center">
-                                            <span aria-hidden="true" className="h-0.5 grow rounded bg-gray-100 dark:bg-gray-700/75"/>
+                                            <span aria-hidden="true" className="h-0.5 grow rounded bg-gray-100 dark:bg-gray-700/75" />
                                             <span className="rounded-full bg-gray-100 px-3 py-1 text-xs font-medium text-gray-800 dark:bg-gray-700 dark:text-gray-200">
                                                 or login with
                                             </span>
-                                            <span aria-hidden="true" className="h-0.5 grow rounded bg-gray-100 dark:bg-gray-700/75"/>
+                                            <span aria-hidden="true" className="h-0.5 grow rounded bg-gray-100 dark:bg-gray-700/75" />
                                         </div>
                                         {/* END Divider: With Label */}
                                         <div className="grid grid-cols-2 gap-2">
-                                        <a href="" className="btn flex gap-1"><FaGoogle className="text-lg"/> Google</a>
-                                            <a href="" className="btn flex gap-1"><FaFacebook className="text-lg"/> Facebook</a>
+                                            <a href="" className="btn flex gap-1"><FaGoogle className="text-lg" /> Google</a>
+                                            <a href="" className="btn flex gap-1"><FaFacebook className="text-lg" /> Facebook</a>
                                         </div>
                                     </div>
                                 </form>
                             </div>
                             <div className="grow bg-gray-50 p-5 text-center text-sm md:px-16 dark:bg-gray-700/50">Don&apos;t have an account? <Link to="/register" className="font-medium text-blue-600 hover:text-blue-400 dark:text-blue-400 dark:hover:text-blue-300">Register</Link>
                             </div>
+                            {
+                                errStatus && <p className="text-red-500 text-center py-1">{errStatus}</p>
+                            }
                         </div>
                     </section>
                 </div>
